30585ea00d5622e44c785e6c6d8a43f8bf8cc5a7
Do Trung Kien <trungkien.keio@gmail.com>
2019-07-18 17:39:01 +0700

    add lane effect

---
+++ sheep-fight/Assets/Scripts/Lane.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lane : MonoBehaviour
+{
+    public GameObject laneEffect;
+
+    void OnMouseDown()
+    {
+        var effect = Instantiate(laneEffect, transform.position, Quaternion.identity, transform);
+        GameObject.Destroy(effect, 0.15f);
+    }
+}
