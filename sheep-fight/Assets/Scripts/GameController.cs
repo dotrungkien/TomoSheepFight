beaa083bfce31f6763a21ff00790b4522fc512cb
Do Trung Kien <trungkien.keio@gmail.com>
2019-07-19 13:49:29 +0700

    implement spawn sheep logic

---
+++ sheep-fight/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Sheep[] whiteSheeps;
+    public Sheep[] blackSheeps;
+    public Transform[] wSpawnPositions;
+    public Transform[] bSpawnPositions;
+
+    void SpawnSheeps(bool isWhite, int sheepIndex, int laneIndex)
+    {
+        if (isWhite)
+        {
+            Sheep sheep = Instantiate<Sheep>(whiteSheeps[sheepIndex], wSpawnPositions[laneIndex].position, Quaternion.identity, wSpawnPositions[laneIndex]);
+        }
+        else
+        {
+            Sheep sheep = Instantiate<Sheep>(blackSheeps[sheepIndex], bSpawnPositions[laneIndex].position, Quaternion.identity, bSpawnPositions[laneIndex]);
+            sheep.direction = -1;
+        }
+    }
+
+}
