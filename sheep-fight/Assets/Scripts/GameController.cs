d7d4bb1b5e0bb0d934d0b0bfc870d98a614f09a8
Do Trung Kien <trungkien.keio@gmail.com>
2019-07-22 14:05:49 +0700

    update spawn logic

--- sheep-fight/Assets/Scripts/GameController.cs
+++ sheep-fight/Assets/Scripts/GameController.cs
@@ -6,8 +6,51 @@ public class GameController : MonoBehaviour
 {
     public Sheep[] whiteSheeps;
     public Sheep[] blackSheeps;
+    public GameObject[] sheepIcons;
     public Transform[] wSpawnPositions;
     public Transform[] bSpawnPositions;
+
+    public float coolDown = 2.0f;
+    public bool isReady = false;
+
+    private GameObject sheepIcon;
+
+    public void StartGame()
+    {
+        isReady = true;
+    }
+
+    private void Update()
+    {
+        if (!isReady)
+        {
+            coolDown -= Time.deltaTime;
+            if (coolDown <= 0f)
+            {
+                coolDown = 0;
+                isReady = true;
+            }
+        }
+    }
+
+    public void SpawnLane(int laneIndex)
+    {
+        if (isReady)
+        {
+            SpawnSheeps(true, 2, laneIndex);
+        }
+        else
+        {
+            PrepareSheep(2, laneIndex);
+        }
+    }
+
+    void PrepareSheep(int sheepIndex, int laneIndex)
+    {
+         = Instantiate(sheepIcons[sheepIndex], wSpawnPositions[laneIndex].position, Quaternion.identity, wSpawnPositions[laneIndex]);
+        GameObject.Destroy(sheep, coolDown);
+    }
+
 
     void SpawnSheeps(bool isWhite, int sheepIndex, int laneIndex)
     {
@@ -22,4 +65,9 @@ public class GameController : MonoBehaviour
         }
     }
 
+    public void CoolDownReset()
+    {
+        coolDown = 2.0f;
+        isReady = false;
+    }
 }
--- sheep-fight/Assets/Scripts/Lane.cs
+++ sheep-fight/Assets/Scripts/Lane.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Lane : MonoBehaviour
 {
+    public GameController gameController;
     public GameObject laneEffect;
+    public int index;
 
     void OnMouseDown()
     {
         var effect = Instantiate(laneEffect, transform.position, Quaternion.identity, transform);
         GameObject.Destroy(effect, 0.15f);
+        gameController.SpawnLane(index);
     }
 }
