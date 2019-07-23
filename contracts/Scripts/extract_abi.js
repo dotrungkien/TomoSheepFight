3e232c73e2eb856a68637ba5449a081ff9dc44a7
Do Trung Kien <trungkien.keio@gmail.com>
2019-07-23 14:39:59 +0700

    feat: extract abi

--- contracts/Scripts/extract_abi.js
+++ contracts/Scripts/extract_abi.js
@@ -1,17 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 const contracts = path.resolve(__dirname, '../build/contracts/');
-const unityAbis = path.resolve(__dirname, '../../Assets/Contracts/');
+const unityAbis = path.resolve(__dirname, '../../sheep-fight/Assets/Contracts/');
 
-module.exports = async function() {
+module.exports = function() {
   let builtContracts = fs.readdirSync(contracts);
   builtContracts.forEach((contract) => {
-    if (contract === 'Migrations.json') return;
+    if (contract === 'Migrations.json' || contract === 'SafeMath.json') return;
+    console.log('extracting contract ', contract);
     const name = contract.split('.')[0];
     let json = JSON.parse(fs.readFileSync(path.resolve(contracts, contract)));
     let { abi, networks } = json;
     if (!Object.keys(networks).length) return;
     fs.writeFileSync(path.resolve(unityAbis, contract), JSON.stringify(json.abi));
-    fs.writeFileSync(path.resolve(unityAbis, name + 'Address.txt'), networks['89'].address);
+    // fs.writeFileSync(path.resolve(unityAbis, name + 'Address.txt'), networks['89'].address);
+    fs.writeFileSync(path.resolve(unityAbis, name + 'Address.txt'), networks['4447'].address);
   });
 };
