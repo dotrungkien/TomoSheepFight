fd0e8e8242a36d588a7b20a2744b8002d98ee68a
Do Trung Kien <trungkien.keio@gmail.com>
2019-08-08 11:11:24 +0700

    update project

--- contracts/contracts/SheepFight.sol
+++ contracts/contracts/SheepFight.sol
@@ -1,19 +1,28 @@
 pragma solidity 0.5.0;
 
 contract SheepFight {
+    struct Battle {
+        string id;
+        address leftPlayer;
+        address rightPlayer;
+        uint result; // 0 - playing, 1 - leftwin, 2 - rightwin
+    }
+
     mapping (address => bool) public isPlaying;
 
     uint public betValue = 1 ether;
+    address[] public waitlist;
+    Battle[] public battles;
 
-    modifier onlyReady()
-    {
-        require(!isPlaying[msg.sender], 'player must not be in game');
+    event StartBattle(address indexed leftPlayer, address indexed rightPlayer);
+
+    modifier onlyReady() {
+        require(!isPlaying[msg.sender], "player must not be in game");
         _;
     }
 
-    modifier onlyPlaying()
-    {
-        require(isPlaying[msg.sender], 'player must be in game');
+    modifier onlyPlaying() {
+        require(isPlaying[msg.sender], "player must be in game");
         _;
     }
 
@@ -22,7 +31,7 @@ contract SheepFight {
         payable
         onlyReady
     {
-        require(msg.value >= betValue, 'must bet 1 value');
+        require(msg.value >= betValue, "must bet 1 value");
         if (msg.value > betValue) {
             msg.sender.transfer(msg.value - betValue);
         }
@@ -34,7 +43,7 @@ contract SheepFight {
         onlyPlaying
     {
         if (isWon) {
-            require(address(this).balance >= 2*betValue, 'insufficient balance');
+            require(address(this).balance >= 2*betValue, "insufficient balance");
             msg.sender.transfer(2*betValue);
         }
         isPlaying[msg.sender] = false;
