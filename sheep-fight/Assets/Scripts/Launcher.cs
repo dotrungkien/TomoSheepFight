1b041b35ee227baba971214e34a66fc9f59fe222
Do Trung Kien <trungkien.keio@gmail.com>
2019-08-19 16:05:02 +0700

    init launcher

--- sheep-fight/Assets/Scripts/Launcher.cs
+++ sheep-fight/Assets/Scripts/Launcher.cs
@@ -8,12 +8,6 @@ public class Launcher : MonoBehaviourPunCallbacks
     [SerializeField]
     private byte maxPlayersPerRoom = 2;
 
-    [SerializeField]
-    private GameObject controlPanel;
-
-    [SerializeField]
-    private GameObject progressLabel;
-
     bool isConnecting;
 
     void Awake()
@@ -23,15 +17,12 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     void Start()
     {
-        progressLabel.SetActive(false);
-        controlPanel.SetActive(true);
+        Connect();
     }
 
     public void Connect()
     {
         isConnecting = true;
-        progressLabel.SetActive(true);
-        controlPanel.SetActive(false);
         // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
         if (PhotonNetwork.IsConnected)
         {
@@ -48,7 +39,7 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public override void OnConnectedToMaster()
     {
-        Debug.Log("OnConnectedToMaster() was called by PUN");
+        Debug.Log("OnConnectedToMaster()");
         if (isConnecting)
         {
             PhotonNetwork.JoinRandomRoom();
@@ -57,27 +48,22 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     public override void OnDisconnected(DisconnectCause cause)
     {
-        progressLabel.SetActive(false);
-        controlPanel.SetActive(true);
-        Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason {0}", cause);
+        Debug.LogWarningFormat("OnDisconnected() with reason {0}", cause);
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        Debug.Log("OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
-
-        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        Debug.Log("OnJoinRandomFailed. No random room available, so we create one.");
         PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
     }
 
     public override void OnJoinedRoom()
     {
-        Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room.");
+        Debug.Log("OnJoinedRoom(). Now this client is in a room.");
         if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
         {
-            Debug.Log("We load the 'Room for 1'");
-
-            PhotonNetwork.LoadLevel("Room for 1");
+            Debug.LogFormat("Current room {0}", PhotonNetwork.CurrentRoom.Name);
+            // PhotonNetwork.LoadLevel("Room for 1");
         }
     }
 
