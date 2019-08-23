f9b03718e34b44102a331e53fc2345643da66d97
Do Trung Kien <trungkien.keio@gmail.com>
2019-08-23 08:48:43 +0700

    update contract

--- contracts/contracts/SheepFight.sol
+++ contracts/contracts/SheepFight.sol
@@ -1,31 +1,38 @@
 pragma solidity 0.5.0;
 
 contract SheepFight {
+    struct Game {
+        string id;
+        address leftPlayer;
+        address secondPlayer;
+    }
+
+    Game[] public games;
+
     mapping (address => bool) public isPlaying;
-    mapping (address => string) public playerToGame;
+    mapping (address => uint) public playerToGame;
 
     uint public betValue = 1 ether;
 
-    event StartBattle(address indexed leftPlayer, address indexed rightPlayer);
-
-    modifier onlyReady() {
-        require(!isPlaying[msg.sender], "player must not be in game");
-        require(compareStringsbyBytes(playerToGame[msg.sender], ""), "start wrong game ID");
-        _;
-    }
-
-    modifier onlyPlaying(string memory gameID) {
-        require(isPlaying[msg.sender], "player must be in game");
-        require(compareStringsbyBytes(playerToGame[msg.sender], gameID), "end wrong game ID");
-        _;
+    function searchGame(string memory gameID)
+        public
+        returns (int)
+    {
+        for (uint i = 0; i < games.length; i++)
+        {
+            if (compareStringsbyBytes(games[i].id, gameID)) return int(i);
+        }
+        return -1;
     }
 
     function play(string calldata gameID)
         external
         payable
-        onlyReady
     {
         require(msg.value >= betValue, "must bet 1 value");
+        require(!isPlaying[msg.sender], "player must not be in game");
+
+
         if (msg.value > betValue) {
             msg.sender.transfer(msg.value - betValue);
         }
@@ -35,8 +42,8 @@ contract SheepFight {
 
     function endGame(string calldata gameID, bool isWon)
         external
-        onlyPlaying(gameID)
     {
+        require(isPlaying[msg.sender], "player must be in game");
         if (isWon) {
             require(address(this).balance >= 2*betValue, "insufficient balance");
             msg.sender.transfer(2*betValue);
