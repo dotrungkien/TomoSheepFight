1445935eeb9ef62a091c94d4071a12e3e951f05b
Do Trung Kien <trungkien.keio@gmail.com>
2019-08-23 09:00:56 +0700

    update contract

--- contracts/contracts/SheepFight.sol
+++ contracts/contracts/SheepFight.sol
@@ -4,7 +4,7 @@ contract SheepFight {
     struct Game {
         string id;
         address leftPlayer;
-        address secondPlayer;
+        address rightPlayer;
     }
 
     Game[] public games;
@@ -14,15 +14,19 @@ contract SheepFight {
 
     uint public betValue = 1 ether;
 
+    constructor () public {
+        games.push(Game("123456", address(0), address(0)));
+    }
+
     function searchGame(string memory gameID)
         public
-        returns (int)
+        returns (uint)
     {
         for (uint i = 0; i < games.length; i++)
         {
-            if (compareStringsbyBytes(games[i].id, gameID)) return int(i);
+            if (compareStringsbyBytes(games[i].id, gameID)) return i;
         }
-        return -1;
+        return 0;
     }
 
     function play(string calldata gameID)
@@ -33,30 +37,49 @@ contract SheepFight {
         require(!isPlaying[msg.sender], "player must not be in game");
 
 
-        if (msg.value > betValue) {
-            msg.sender.transfer(msg.value - betValue);
-        }
         isPlaying[msg.sender] = true;
-        playerToGame[msg.sender] = gameID;
+        uint gameIdx = searchGame(gameID);
+        if (gameIdx == 0) {
+            CreateGame(gameID);
+        } else {
+            JoinGame(gameIdx);
+        }
+        if (msg.value > betValue) msg.sender.transfer(msg.value - betValue);
     }
+
+    function CreateGame(string memory gameID)
+        internal
+        returns (uint)
+    {
+        Game memory newGame = Game(gameID, msg.sender, address(0));
+        uint latestGame = games.push(newGame);
+        playerToGame[msg.sender] = latestGame;
+        return latestGame;
+    }
+
+    function JoinGame(uint gameIdx)
+        internal
+    {
+        Game storage game = games[gameIdx];
+        game.rightPlayer = msg.sender;
+        playerToGame[msg.sender] = gameIdx;
+    }
+
 
     function endGame(string calldata gameID, bool isWon)
         external
     {
         require(isPlaying[msg.sender], "player must be in game");
-        if (isWon) {
-            require(address(this).balance >= 2*betValue, "insufficient balance");
-            msg.sender.transfer(2*betValue);
-        }
+        if (isWon) reward(msg.sender);
         isPlaying[msg.sender] = false;
-        playerToGame[msg.sender] = "";
+        playerToGame[msg.sender] = 0;
     }
 
     function forceEndGame()
         external
     {
         isPlaying[msg.sender] = false;
-        playerToGame[msg.sender] = "";
+        playerToGame[msg.sender] = -1;
     }
 
     function compareStringsbyBytes(string memory s1, string memory  s2)
@@ -65,6 +88,14 @@ contract SheepFight {
         returns(bool)
     {
         return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
+    }
+
+    function reward(address payable to)
+        public
+        payable
+    {
+        require(address(this).balance >= 2*betValue, "insufficient balance");
+        to.transfer(2*betValue);
     }
 
     function () external payable {}
